b0e28db86ed179270ffd093de3048c46b1a1740c
thisisvv <58626061+thisisvv@users.noreply.github.com>
2022-12-16 16:54:16 +0100

    + Validation for registration

--- src/main/java/wiss/agile426/sprint01/controller/UserController.java
+++ src/main/java/wiss/agile426/sprint01/controller/UserController.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import wiss.agile426.sprint01.model.User;
-import wiss.agile426.sprint01.repository.ProjectRepository;
 import wiss.agile426.sprint01.repository.UserRepository;
 
 
 /**
- * TODO: Everything xD
+ * TODO: Login + Authenticator (add dependency to pom.xml)
  */
 
 
@@ -24,16 +23,33 @@ public class UserController {
     /**
      *
      * @Insomnia: POST http://127.0.0.1:8080/user/signup
+     *
      *        {
      * 			"name": "Bob",
      * 			"username": "xXbob69Xx",
      * 		  "email": "bob@bob.ch",
      * 		  "password": "secretbob"
      *    }
+     *
      *    @return: "User registered successfully"
      */
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto) {
+
+        if (userRepository.existsByUsername(signUpDto.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Username is already taken!");
+        }
+
+        /* why dis not work? :(
+        if (userRepository.existsByEmail(signUpDto.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Email is already in use!");
+        }
+    */
+
         User user = new User();
         user.setName(signUpDto.getName());
         user.setUsername(signUpDto.getUsername());
@@ -42,4 +58,5 @@ public class UserController {
         userRepository.save(user);
         return ResponseEntity.ok("User registered successfully");
     }
+
 }
--- src/main/java/wiss/agile426/sprint01/repository/UserRepository.java
+++ src/main/java/wiss/agile426/sprint01/repository/UserRepository.java
@@ -1,7 +1,17 @@
 package wiss.agile426.sprint01.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import wiss.agile426.sprint01.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    //existsByUsername
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.username = ?1")
+    boolean existsByUsername(String username);
+
+    /*  why dis not work? :(
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = ?1")
+    boolean existsByEmail(String email);
+ */
+
 }
