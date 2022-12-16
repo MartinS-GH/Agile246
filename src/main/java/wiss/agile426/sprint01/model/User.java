5a710bb64b9fcb5cc6f5204d1b4ee7652548c83a
thisisvv <thisisvv@gmail.com>
2022-12-16 13:39:41 +0100

    Fixed Entity ID -> GenerationType.IDENTITY Added inital .sql file -> newDB.sql

--- src/main/java/wiss/agile426/sprint01/model/User.java
+++ src/main/java/wiss/agile426/sprint01/model/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "user")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
