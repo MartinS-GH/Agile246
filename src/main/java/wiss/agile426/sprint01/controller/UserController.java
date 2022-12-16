e92e1ea80ea023142b7d8bb99bca308a237a0b5c
thisisvv <58626061+thisisvv@users.noreply.github.com>
2022-12-16 16:25:39 +0100

    + SignUpDto.java + Code for /signup

--- src/main/java/wiss/agile426/sprint01/controller/UserController.java
+++ src/main/java/wiss/agile426/sprint01/controller/UserController.java
@@ -1,7 +1,9 @@
 package wiss.agile426.sprint01.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import wiss.agile426.sprint01.model.User;
 import wiss.agile426.sprint01.repository.ProjectRepository;
 import wiss.agile426.sprint01.repository.UserRepository;
 
@@ -19,5 +21,14 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto) {
+        User user = new User();
+        user.setName(signUpDto.getName());
+        user.setUsername(signUpDto.getUsername());
+        user.setEmail(signUpDto.getEmail());
+        user.setPassword(signUpDto.getPassword());
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
 }
--- src/main/java/wiss/agile426/sprint01/model/User.java
+++ src/main/java/wiss/agile426/sprint01/model/User.java
@@ -6,52 +6,54 @@ import jakarta.persistence.*;
 @Table(name = "user")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Column(name = "first_name", length = 24)
-    private String firstName;
+    @Column(name = "name", length = 24)
+    private String name;
 
-    @Column(name = "last_name", nullable = false, length = 24)
-    private String lastName;
+    @Column(name = "username", nullable = false, length = 24)
+    private String username;
 
-    @Column(name = "e_mail", unique = true, length = 55)
+
+    @Column(name = "email", unique = true, length = 55)
     private String eMail;
 
-    @Column(name = "telephon_number", unique = true)
-    private String telephonNumber;
 
-    public String getTelephonNumber() {
-        return telephonNumber;
+    @Column(name = "password", nullable = false, length = 64)
+    private String password;
+
+    public String getPassword() {
+        return password;
     }
 
-    public void setTelephonNumber(String telephonNumber) {
-        this.telephonNumber = telephonNumber;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getEMail() {
+    public String getEmail() {
         return eMail;
     }
 
-    public void setEMail(String eMail) {
+    public void setEmail(String eMail) {
         this.eMail = eMail;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getName() {
+        return name;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public Integer getId() {
