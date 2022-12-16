480a5335cf1b9ebf478be3ef0d30fe79b7041c8f
Lucas <80749221+LucasCGG@users.noreply.github.com>
2022-12-16 21:07:23 +0100

    Updated projectcontroller

--- src/main/java/wiss/agile426/sprint01/controller/ProjectController.java
+++ src/main/java/wiss/agile426/sprint01/controller/ProjectController.java
@@ -1,7 +1,37 @@
 package wiss.agile426.sprint01.controller;
 
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import wiss.agile426.sprint01.Sprint01Application;
+import wiss.agile426.sprint01.model.Project;
+import wiss.agile426.sprint01.repository.ProjectRepository;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+
+
+@CrossOrigin
+@RestController
+@RequestMapping(path = "/project")
 public class ProjectController {
-    /**
-     * TODO: Everything xD
-     */
+    Sprint01Application application;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @PostMapping(path = "")
+    public @ResponseBody ResponseEntity<String> addProject(@Valid @RequestBody Project project){
+        projectRepository.save(project);
+        return ResponseEntity.status(200).body("Project was succesfully addet");
+    }
+
+    @GetMapping(path = "")
+    public @ResponseBody Stream<Project> getAllProjects(){
+        List<Project> projectList = projectRepository.findAll();
+        return projectList.stream();
+    }
 }
--- src/main/java/wiss/agile426/sprint01/model/User.java
+++ src/main/java/wiss/agile426/sprint01/model/User.java
@@ -10,8 +10,18 @@ public class User {
     @Column(name = "id", nullable = false)
     private Integer id;
 
+<<<<<<< Updated upstream
     @Column(name = "name", length = 24)
     private String name;
+=======
+<<<<<<< HEAD
+    @Column(name = "first_name",nullable = false, length = 24)
+    private String firstName;
+=======
+    @Column(name = "name", length = 24)
+    private String name;
+>>>>>>> b0e28db86ed179270ffd093de3048c46b1a1740c
+>>>>>>> Stashed changes
 
     @Column(name = "username", nullable = false, length = 24)
     private String username;
@@ -21,11 +31,84 @@ public class User {
     private String eMail;
 
 
+<<<<<<< Updated upstream
     @Column(name = "password", nullable = false, length = 64)
     private String password;
 
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return eMail;
+    }
+
+    public void setEmail(String eMail) {
+        this.eMail = eMail;
+=======
+<<<<<<< HEAD
+
+    //GETTER
+    public String getTelephonNumber() {
+        return telephonNumber;
+    }
+    public String getEMail() {
+        return eMail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+>>>>>>> Stashed changes
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+<<<<<<< Updated upstream
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+=======
+
+
+    //SETTER
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public void setEMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public void setTelephonNumber(String telephonNumber) {
+        this.telephonNumber = telephonNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+=======
+    @Column(name = "password", nullable = false, length = 64)
+    private String password;
+
+    public String getPassword() {
+        return password;
+>>>>>>> Stashed changes
     }
 
     public void setPassword(String password) {
@@ -54,11 +137,9 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+>>>>>>> b0e28db86ed179270ffd093de3048c46b1a1740c
     }
 
-    public Integer getId() {
-        return id;
-    }
 
     public void setId(Integer id) {
         this.id = id;
